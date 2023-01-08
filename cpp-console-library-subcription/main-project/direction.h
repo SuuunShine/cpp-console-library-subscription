1553f4a42f87471f0fcede192474b8a04ead85be
SuuunShine <dimalev70@gmail.com>
2023-01-08 15:26:31 +0300

    Реализованно форматирование данных при выводе в консоль

--- cpp-console-library-subcription/main-project/direction.h
+++ cpp-console-library-subcription/main-project/direction.h
@@ -1,1 +1,29 @@
 #pragma once
+#ifndef DIRECTION_H
+#define DIRECTION_H
+#include "constants.h"
+
+struct date
+{
+	int day;
+	int month;
+};
+
+struct direction
+{
+	char direction[MAX_STRING_SIZE];
+};
+struct Speed
+{
+	double speed;
+};
+struct wind
+{
+	date start;
+
+	direction side_world;
+	Speed meters;
+
+};
+#endif
+
