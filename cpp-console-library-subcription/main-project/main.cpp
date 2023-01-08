0ca2f11f5e78fbf13bdecdf3910478f84822a455
SuuunShine <122204979+SuuunShine@users.noreply.github.com>
2023-01-08 14:03:41 +0300

    Добавлен вывод общей информации о задании

---
+++ cpp-console-library-subcription/main-project/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>;
+using namespace std;
+int main(){
+setlocale(LC_ALL, "Russian");
+cout << "Лабораторная работа №9. GIT\n";
+cout << "Вариант 9\n";
+cout << "Левшицкий Дмитрий\n";
+cout << "Группа 11\n";
+system("pause");
+}
