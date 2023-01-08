9567ea28f72e3fb9caa8abc413ca8709b5797aba
SuuunShine <dimalev70@gmail.com>
2023-01-08 14:57:57 +0300

    Добавлены тестовые данные

--- cpp-console-library-subcription/main-project/main.cpp
+++ cpp-console-library-subcription/main-project/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>;
 using namespace std;
 int main(){
-cout << "Lab #9. GIT\n";
-cout << "Variant 9\n";
-cout << "Levshicky Dmitry\n";
-cout << "Group 11\n";
+cout << "Laboratory work #9. GIT\n";
+cout << "Variant #9\n";
+cout << "Author: Levshicky Dmitry\n";
+cout << "Group: 11\n";
 system("pause");
 }
